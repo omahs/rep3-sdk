2bdefdb1048a91f1c3e69df421830d1e494877ab
Jashan Shetty <43118727+eth-jashan@users.noreply.github.com>
2022-05-09 11:49:50 +0530

    Graph URL Change

--- src/constants.ts
+++ src/constants.ts
@@ -11,5 +11,6 @@ export const deployed_address = {
 export const BASE_URL = {
   relayer: 'https://staging.api.drepute.xyz:5001/eth/relay',
   subgraph:
-    'https://api.thegraph.com/subgraphs/id/QmctWyJKqKwU7XJmxK8c1MA3BW4YPEzN8LbSmECcP8N6gk',
+    // 'https://api.thegraph.com/subgraphs/id/QmctWyJKqKwU7XJmxK8c1MA3BW4YPEzN8LbSmECcP8N6gk',
+    'https://api.thegraph.com/subgraphs/name/eth-jashan/final-drepute',
 };
