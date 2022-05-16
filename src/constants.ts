9a8059f18d3b696e240954be9c405acfbcfd8fe8
Jashan Shetty <43118727+eth-jashan@users.noreply.github.com>
2022-05-17 03:12:46 +0530

    Subgraph changes

--- src/constants.ts
+++ src/constants.ts
@@ -4,12 +4,12 @@ export const networks_ENUM = {
 };
 
 export const deployed_address = {
-  POCP_Forwarder: '0x5F4a85aD13c016A11e2738F28F002f445A4f6673',
-  POCP_Proxy: '0x74Ce96D2e9Ce8D48FF52Ee62BEB9388738113AF5',
+  POCP_Forwarder: '0x025442a46113DdA079dB178E5Cb0714E5ca51FB3',
+  POCP_Proxy: '0x7cd27c57e1CBb81Ba8f3aF35CFB275CB107C897d',
 };
 
 export const BASE_URL = {
   relayer: 'https://staging.api.drepute.xyz:5001/eth/relay',
   subgraph:
-    'https://api.thegraph.com/subgraphs/id/QmXM1pVbWqEeB5iF6Wg5DsUKtaGwZduKhN5H24TvYg1xHG',
+    'https://api.thegraph.com/subgraphs/id/QmTVvWNXEprmdiwXLyGAJ5uub9Q7vBmEuqiX9grtbmT9AZ',
 };
